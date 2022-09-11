c3df0e27778f397dac41b4220523322a2bbade7b
Charles <c_onyemem@live.com>
2022-09-10 18:41:45 -0700

    Completed lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+ 
+    name_list.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+
+
+def assign_rooms(name_list)
+     name_list.map.with_index(1) do |names, idx|
+      "Hello, #{names}! You'll be assigned to room #{idx}!"
+   end
+
+end
+
+def printer(name_list)
+    batch_badge_creator(name_list).each do |messages|
+        puts messages
+    end
+    assign_rooms(name_list).each do |assigned|
+        puts assigned
+    end
+   
+
+end
+
